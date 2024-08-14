8eb338d92bdfeeca3a298bfddecc7869a6a8bdb4
Arjun <alphasva1998@gmail.com>
2024-08-14 11:36:09 +0530

    Bug fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import { check, validationResult } from "express-validator";
 import mongoose from "mongoose";
 
-const PORT = 3000;
+const PORT = 4000;
 const app = express();
 
 const mongoURI =
@@ -33,49 +33,49 @@ const passwordConfirmationCheck = (value, { req }) => {
   return true;
 };
 
-// const now = new Date();
-// const millisTill6PM =
-//   new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18, 0, 0) - now;
-
-// if (millisTill6PM > 0) {
-//   // If there's still some time left, just wait till 6PM
-//   setTimeout(resetIsVoted, millisTill6PM);
-// } else {
-//   // Run immediatly if it's already past 6PM
-//   resetIsVoted();
-// }
-// Modify the opening hour check
 const now = new Date();
-const openingHour = 13; // 1 PM
-const closingHour = 13; // 1 PM
-const closingMinute = 15; // 1:15 PM
+const millisTill6PM =
+  new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18, 0, 0) - now;
 
-const currentHour = now.getHours();
-const currentMinute = now.getMinutes();
-
-const isOpen =
-  currentHour === openingHour &&
-  currentMinute >= 0 &&
-  currentMinute <= closingMinute;
-
-// Adjust the interval for resetting the voting status
-const millisTillClosing =
-  new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    closingHour,
-    closingMinute,
-    0
-  ) - now;
-
-if (millisTillClosing > 0) {
-  // If there's still some time left, just wait till closing time
-  setTimeout(resetIsVoted, millisTillClosing);
+if (millisTill6PM > 0) {
+  // If there's still some time left, just wait till 6PM
+  setTimeout(resetIsVoted, millisTill6PM);
 } else {
-  // Run immediately if it's already past closing time
+  // Run immediatly if it's already past 6PM
   resetIsVoted();
 }
+// Modify the opening hour check
+// const now = new Date();
+// const openingHour = 13; // 1 PM
+// const closingHour = 13; // 1 PM
+// const closingMinute = 15; // 1:15 PM
+
+// const currentHour = now.getHours();
+// const currentMinute = now.getMinutes();
+
+// const isOpen =
+//   currentHour === openingHour &&
+//   currentMinute >= 0 &&
+//   currentMinute <= closingMinute;
+
+// // Adjust the interval for resetting the voting status
+// const millisTillClosing =
+//   new Date(
+//     now.getFullYear(),
+//     now.getMonth(),
+//     now.getDate(),
+//     closingHour,
+//     closingMinute,
+//     0
+//   ) - now;
+
+// if (millisTillClosing > 0) {
+//   // If there's still some time left, just wait till closing time
+//   setTimeout(resetIsVoted, millisTillClosing);
+// } else {
+//   // Run immediately if it's already past closing time
+//   resetIsVoted();
+// }
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -87,123 +87,121 @@ app.get("/login", async (req, res) => {
 
 app.post(
   "/register/submit",
-  [
-    check("username")
-      .isLength({ min: 3 })
-      .withMessage("Username should be minimum 3 characters"),
-    check("email")
-      .isEmail()
-      .custom((value) => {
-        return value.endsWith("@citchennai.net");
-      })
-      .withMessage("Email must be associated with CIT"),
-    check("password")
-      .isLength({ min: 6 })
-      .withMessage("Password should be at least 6 characters long"),
-    check("confirm")
-      .custom(passwordConfirmationCheck)
-      .withMessage("Confirm password does not match with password"),
-  ],
+  // [
+  //   check("username")
+  //     .isLength({ min: 3 })
+  //     .withMessage("Username should be minimum 3 characters"),
+  //   check("email")
+  //     .isEmail()
+  //     .custom((value) => {
+  //       return value.endsWith("@citchennai.net");
+  //     })
+  //     .withMessage("Email must be associated with CIT"),
+  //   check("password")
+  //     .isLength({ min: 6 })
+  //     .withMessage("Password should be at least 6 characters long"),
+  //   check("confirm")
+  //     .custom(passwordConfirmationCheck)
+  //     .withMessage("Confirm password does not match with password"),
+  // ],
   async (req, res) => {
-    var isOpen = false;
-    // const time = new Date().getHours();
-    const time = new Date();
-    const currentHours = time.getHours();
-    const currentMinutes = time.getMinutes();
-    if (currentHours >= 14 && currentMinutes >=0 && currentMinutes<=15) {
-      isOpen = true;
-    } else {
-      isOpen = false;
-    }
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.render("index.ejs", {
-        errors: errors.mapped(),
-      });
-    }
-    if (errors.isEmpty() && !isOpen) {
+    try {
+      // const errors = validationResult(req);
+      // if (!errors.isEmpty()) {
+      //   return res.render("index.ejs", {
+      //     errors: errors.mapped(),
+      //   });
+      // }
+
+      const time = new Date().getHours();
+      const isOpen = time >= 6 && time <= 18;
+
+      if (!isOpen) {
+        return res.render("static.ejs"); // Modify as necessary
+      }
+
       let isExist = await db
         .collection("users")
         .findOne({ email: req.body.email });
-
-      if (isExist === null) {
-        await db.collection("users").insertOne({
-          name: req.body.username,
-          email: req.body.email,
-          password: req.body.password,
-          isVoted: false,
-          isAdmin: false,
-        });
-        let currentFoods = await db.collection("foods").find().toArray();
-        res.render("main.ejs", {
-          choices: currentFoods,
-          userEmail: req.body.email,
-        });
-      } else {
+      if (isExist) {
         return res.json({ message: "User with this email already exists." });
       }
-    } else {
-      res.render("static.ejs"); // DO NOT FORGET TO CHANGE THIS
+
+      await db.collection("users").insertOne({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        isVoted: false,
+        isAdmin: false,
+      });
+
+      let currentFoods = await db.collection("foods").find().toArray();
+      return res.render("main.ejs", {
+        choices: currentFoods,
+        userEmail: req.body.email,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Internal Server Error");
     }
   }
 );
 
 app.post(
   "/login/submit",
-  [
-    check("email")
-      .isEmail()
-      .custom((value) => {
-        return value.endsWith("@citchennai.net");
-      })
-      .withMessage("Email must be associated with CIT"),
-    check("password")
-      .isLength({ min: 6 })
-      .withMessage("Password must be at least 6 characters long"),
-  ],
+  // [
+  //   check("email")
+  //     .isEmail()
+  //     .custom((value) => {
+  //       return value.endsWith("@citchennai.net");
+  //     })
+  //     .withMessage("Email must be associated with CIT"),
+  //   check("password")
+  //     .isLength({ min: 6 })
+  //     .withMessage("Password must be at least 6 characters long"),
+  // ],
   async (req, res) => {
-    var isOpen = false;
-    // const time = new Date().getHours();
-    const time = new Date();
-    const currentHours = time.getHours();
-    const currentMinutes = time.getMinutes();
-    if (currentHours>=14 && currentMinutes>=0 && currentMinutes<=15) {
-      isOpen = true;
-    }
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.render("login.ejs", {
-        errors: errors.mapped(),
-      });
-    }
-    if (errors.isEmpty() && isOpen == true) {
-      const isExist = await db
-        .collection("users")
-        .findOne({ email: req.body.email });
-      if (isExist != null && isExist.isVoted == false) {
-        let currentFoods = await db.collection("foods").find().toArray();
-        res.render("main.ejs", {
-          choices: currentFoods,
-          userEmail: req.body.email,
-        });
-      } else if (isExist != null && isExist.isVoted == true) {
-        res.render("main.ejs", { isVoted: true, userEmail: req.body.email });
-      } else {
-        return res.json({ message: "User with this email does not exist." });
+    try {
+      var isOpen = false;
+      const time = new Date().getHours();
+      if (time >= 6 && time <= 18) {
+        isOpen = true;
       }
-    }
-    if (errors.isEmpty() && isOpen == false) {
-      res.render("main.ejs", { userEmail: req.body.email }); //CHANGE THIS
+      // const errors = validationResult(req);
+      // if (!errors.isEmpty()) {
+      //   res.render("login.ejs", {
+      //     errors: errors.mapped(),
+      //   });
+      // }
+      // if (errors.isEmpty() && isOpen == true) {
+      if (isOpen == true) {
+        const isExist = await db
+          .collection("users")
+          .findOne({ email: req.body.email });
+        if (isExist != null && isExist.isVoted == false) {
+          let currentFoods = await db.collection("foods").find().toArray();
+          res.render("main.ejs", {
+            choices: currentFoods,
+            userEmail: req.body.email,
+          });
+        } else if (isExist != null && isExist.isVoted == true) {
+          res.render("main.ejs", { isVoted: true, userEmail: req.body.email });
+        } else {
+          return res.json({ message: "User with this email does not exist." });
+        }
+      }
+      if (errors.isEmpty() && isOpen == false) {
+        res.render("main.ejs", { userEmail: req.body.email }); //CHANGE THIS
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 );
 
 app.get("/home", async (req, res) => {
-  // const time = new Date().getHours();
-  const time = new Date();
-  const currentHours = time.getHours();
-  const currentMinutes = time.getMinutes();
-  if (currentHours>=14 && currentMinutes>=0 && currentMinutes<=15) {
+  const time = new Date().getHours();
+  if (time >= 6 && time <= 18) {
     let currentFoods = await db.collection("foods").find().toArray();
     let voted = await db
       .collection("users")
@@ -269,52 +267,46 @@ var choicesForTheDay = [
   { name: "vada-curry", votes: 0 },
   { name: "idly", votes: 0 },
 ];
+~(
+  // setInterval(selectRandomFood, 10000);
 
-// setInterval(selectRandomFood, 10000);
-
-app.get("/vote", async (req, res) => {
-  // let time = new Date().getHours();
-  const time = new Date();
-  const currentHours = time.getHours();
-  const currentMinutes = time.getMinutes();
-  if (currentHours >= 14 && currentMinutes >= 0 && currentMinutes<=15) {
-    try {
-      const userEmail = req.query.email.toLocaleLowerCase();
-      const user = await db.collection("users").findOne({ email: userEmail });
-      if (!user) {
-        return res.status(404).send("User not found");
-      }
-      if (user.isVoted == false) {
-        const foodName = req.query.food;
-        const result = await db
-          .collection("foods")
-          .updateOne({ name: foodName }, { $inc: { votes: 1 } });
-        if (result.modifiedCount == 0) {
-          return res.status(404).send("Food not found");
+  app.get("/vote", async (req, res) => {
+    let time = new Date().getHours();
+    if (time >= 6 && time <= 18) {
+      try {
+        const userEmail = req.query.email.toLocaleLowerCase();
+        const user = await db.collection("users").findOne({ email: userEmail });
+        if (!user) {
+          return res.status(404).send("User not found");
         }
-        await db
-          .collection("users")
-          .updateOne({ email: userEmail }, { $set: { isVoted: true } });
-        res.render("main.ejs", { isVoted: true, userEmail: userEmail });
-      } else {
-        res.render("main.ejs", { isVoted: true, userEmail: userEmail });
+        if (user.isVoted == false) {
+          const foodName = req.query.food;
+          const result = await db
+            .collection("foods")
+            .updateOne({ name: foodName }, { $inc: { votes: 1 } });
+          if (result.modifiedCount == 0) {
+            return res.status(404).send("Food not found");
+          }
+          await db
+            .collection("users")
+            .updateOne({ email: userEmail }, { $set: { isVoted: true } });
+          res.render("main.ejs", { isVoted: true, userEmail: userEmail });
+        } else {
+          res.render("main.ejs", { isVoted: true, userEmail: userEmail });
+        }
+      } catch (err) {
+        res.json({ message: err.message });
       }
-    } catch (err) {
-      res.json({ message: err.message });
+    } else {
+      res.render("static.ejs");
     }
-  } else {
-    res.render("static.ejs");
-  }
-});
+  })
+);
 
 app.get("/calculateResult", async (req, res) => {
-  // const time = new Date().getHours();
-  const time = new Date();
-  const currentHours = time.getHours();
-  const currentMinutes = time.getMinutes();
+  const time = new Date().getHours();
   try {
-    // if (time >= 18 || time < 6) {
-      if(currentHours >=14 && currentMinutes >15){
+    if (time >= 18 || time < 6) {
       const winner = await db
         .collection("foods")
         .aggregate([{ $sort: { votes: -1 } }, { $limit: 1 }])
@@ -364,7 +356,9 @@ async function selectRandomFood() {
 
 async function resetIsVoted() {
   try {
-    await db.collection("users").updateMany({}, { $set: { isVoted: false } });
+    await db
+      .collection("users")
+      .updateMany({}, { $set: { isVoted: false } }, { timeout: false });
     console.log("Reset isVoted is successful");
   } catch (e) {
     console.log("Error resetting isVoted", e);
